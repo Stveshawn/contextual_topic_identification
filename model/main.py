0411549912fdacd599899d0b7ff3bbeab79316b2
Steve Shao <stveshawn@ruc.edu.cn>
2020-02-07 06:09:49 +0000

    fixing .gitignore

--- model/main.py
+++ model/main.py
@@ -12,16 +12,16 @@ import argparse
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--fpath', default='../data/steam_reviews.csv')
+    parser.add_argument('--fpath', default='../../data/steam_reviews.csv')
     parser.add_argument('--ntopic', default=10)
     args = parser.parse_args()
 
     data = pd.read_csv(args.fpath)
     data = data.fillna('')  # only the comments has NaN's
     rws = data.review
-    sentences, token_lists, idx_in = preprocess(rws, samp_size=100)
+    sentences, token_lists, idx_in = preprocess(rws, samp_size=10000)
     # Define the topic model object
-    tm = Topic_Model(k = args.ntopic, method = 'LDA_BERT')
+    tm = Topic_Model(k = args.ntopic, method = 'TFIDF')
     # Fit the topic model by chosen method
     tm.fit(sentences, token_lists)
     # Evaluate using metrics
